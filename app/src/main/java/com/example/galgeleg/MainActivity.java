267b5509859ae3689968711983dbf2e881ada272
s175565 <s175565@student.dtu.dk>
2019-10-12 13:25:24 +0200

    menu beautified

--- app/src/main/java/com/example/galgeleg/MainActivity.java
+++ app/src/main/java/com/example/galgeleg/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.galgeleg;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button startBtn, highscoreBtn, helpBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startBtn = findViewById(R.id.startBtn);
+        highscoreBtn = findViewById(R.id.highscoreBtn);
+        helpBtn = findViewById(R.id.helpBtn);
+
+        startBtn.setOnClickListener(this);
+        highscoreBtn.setOnClickListener(this);
+        helpBtn.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == startBtn){
+            Log.i("myInfoTag", "startBtn clicked");
+        }
+        else if (v == highscoreBtn){
+            Log.i("myInfoTag", "highscoreBtn clicked");
+        }
+        else {
+            Log.i("myInfoTag", "helpBtn clicked");
+        }
     }
 }
