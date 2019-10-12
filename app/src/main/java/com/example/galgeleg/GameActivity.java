aedef00ef486f35f1dca02a0248844fdbee542f5
s175565 <s175565@student.dtu.dk>
2019-10-12 22:00:21 +0200

    letter btns implemented in game

--- app/src/main/java/com/example/galgeleg/GameActivity.java
+++ app/src/main/java/com/example/galgeleg/GameActivity.java
@@ -2,9 +2,13 @@ package com.example.galgeleg;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputConnection;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,16 +16,24 @@ import androidx.appcompat.app.AppCompatActivity;
 public class GameActivity extends AppCompatActivity implements View.OnClickListener {
     private Button btn;
     private ImageView imageView;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_activity);
 
-        btn = findViewById(R.id.button);
-        btn.setOnClickListener(this);
 
-        imageView = findViewById(R.id.imageView);
+        //imageView = findViewById(R.id.imageView);
+
+        EditText editText = (EditText) findViewById(R.id.editText);
+        MyKeyboard keyboard = (MyKeyboard) findViewById(R.id.keyboard);
+        editText.setRawInputType(InputType.TYPE_CLASS_TEXT);
+        editText.setTextIsSelectable(true);
+
+        InputConnection ic = editText.onCreateInputConnection(new EditorInfo());
+        keyboard.setInputConnection(ic);
+
 
 
     }
---
+++ app/src/main/java/com/example/galgeleg/MyKeyboard.java
@@ -0,0 +1,78 @@
+package com.example.galgeleg;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.SparseArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.inputmethod.InputConnection;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class MyKeyboard extends LinearLayout implements View.OnClickListener {
+
+    private Button qBtn, wBtn, eBtn, rBtn, tBtn, yBtn, uBtn, iBtn, oBtn, pBtn, åBtn,
+                    aBtn, sBtn, dBtn, fBtn, gBtn, hBtn, jBtn, kBtn, lBtn, æBtn, øBtn,
+                        zBtn, xBtn, cBtn, vBtn, bBtn, nBtn, mBtn;
+
+    private SparseArray<String> keyValues = new SparseArray<>();
+    private InputConnection inputConnection;
+
+    public MyKeyboard(Context context) {
+        this(context, null, 0);
+    }
+
+    public MyKeyboard(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public MyKeyboard(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        LayoutInflater.from(context).inflate(R.layout.keyboard, this, true);
+        qBtn = findViewById(R.id.qBtn); qBtn.setOnClickListener(this); keyValues.put(R.id.qBtn, "q");
+        wBtn = findViewById(R.id.wBtn); wBtn.setOnClickListener(this); keyValues.put(R.id.wBtn, "w");
+        eBtn = findViewById(R.id.eQtn); eBtn.setOnClickListener(this); keyValues.put(R.id.eQtn, "e");
+        rBtn = findViewById(R.id.rQtn); rBtn.setOnClickListener(this); keyValues.put(R.id.rQtn, "r");
+        tBtn = findViewById(R.id.tBtn); tBtn.setOnClickListener(this); keyValues.put(R.id.tBtn, "t");
+        yBtn = findViewById(R.id.yBtn); yBtn.setOnClickListener(this); keyValues.put(R.id.yBtn, "y");
+        uBtn = findViewById(R.id.uBtn); uBtn.setOnClickListener(this); keyValues.put(R.id.uBtn, "u");
+        iBtn = findViewById(R.id.iBtn); iBtn.setOnClickListener(this); keyValues.put(R.id.iBtn, "i");
+        oBtn = findViewById(R.id.oBtn); oBtn.setOnClickListener(this); keyValues.put(R.id.oBtn, "o");
+        pBtn = findViewById(R.id.pBtn); pBtn.setOnClickListener(this); keyValues.put(R.id.pBtn, "p");
+        åBtn = findViewById(R.id.åBtn); åBtn.setOnClickListener(this); keyValues.put(R.id.åBtn, "å");
+        aBtn = findViewById(R.id.aBtn); aBtn.setOnClickListener(this); keyValues.put(R.id.aBtn, "a");
+        sBtn = findViewById(R.id.sBtn); sBtn.setOnClickListener(this); keyValues.put(R.id.sBtn, "s");
+        dBtn = findViewById(R.id.dBtn); dBtn.setOnClickListener(this); keyValues.put(R.id.dBtn, "d");
+        fBtn = findViewById(R.id.fBtn); fBtn.setOnClickListener(this); keyValues.put(R.id.fBtn, "f");
+        gBtn = findViewById(R.id.gBtn); gBtn.setOnClickListener(this); keyValues.put(R.id.gBtn, "g");
+        hBtn = findViewById(R.id.hBtn); hBtn.setOnClickListener(this); keyValues.put(R.id.hBtn, "h");
+        jBtn = findViewById(R.id.jBtn); jBtn.setOnClickListener(this); keyValues.put(R.id.jBtn, "j");
+        kBtn = findViewById(R.id.kBtn); kBtn.setOnClickListener(this); keyValues.put(R.id.kBtn, "k");
+        lBtn = findViewById(R.id.lBtn); lBtn.setOnClickListener(this); keyValues.put(R.id.lBtn, "l");
+        æBtn = findViewById(R.id.æBtn); æBtn.setOnClickListener(this); keyValues.put(R.id.æBtn, "æ");
+        øBtn = findViewById(R.id.øBtn); øBtn.setOnClickListener(this); keyValues.put(R.id.øBtn, "ø");
+        zBtn = findViewById(R.id.zBtn); zBtn.setOnClickListener(this); keyValues.put(R.id.zBtn, "z");
+        xBtn = findViewById(R.id.xBtn); xBtn.setOnClickListener(this); keyValues.put(R.id.xBtn, "x");
+        cBtn = findViewById(R.id.cBtn); cBtn.setOnClickListener(this); keyValues.put(R.id.cBtn, "c");
+        vBtn = findViewById(R.id.vBtn); vBtn.setOnClickListener(this); keyValues.put(R.id.vBtn, "v");
+        bBtn = findViewById(R.id.bBtn); bBtn.setOnClickListener(this); keyValues.put(R.id.bBtn, "b");
+        nBtn = findViewById(R.id.nBtn); nBtn.setOnClickListener(this); keyValues.put(R.id.nBtn, "n");
+        mBtn = findViewById(R.id.mBtn); mBtn.setOnClickListener(this); keyValues.put(R.id.mBtn, "m");
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (inputConnection == null) return;
+
+        String value = keyValues.get(view.getId());
+        inputConnection.commitText(value, 1);
+    }
+
+    public void setInputConnection(InputConnection ic) {
+        inputConnection = ic;
+    }
+}
