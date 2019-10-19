46f97967c8c9aa7ac730fb8d7fa1530d673f5a19
s175565 <s175565@student.dtu.dk>
2019-10-19 18:05:50 +0200

    added Nordfalk's test

--- app/src/test/java/com/example/galgeleg/ExampleUnitTest.java
+++ app/src/test/java/com/example/galgeleg/ExampleUnitTest.java
@@ -1,8 +1,12 @@
 package com.example.galgeleg;
 
+import com.example.galgeleg.model.Logic;
+
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -11,7 +15,30 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void afprøvMedSkovsnegl() {
+        Logic game = Logic.getInstance();
+        game.getWordLibrary().clear();
+        game.getWordLibrary().add("skowsnegl");
+
+        game.restart();
+
+        game.guessedLetter('e');
+        game.guessedLetter('s');
+        assertEquals(0, game.getWrongGuess());
+
+        assertEquals("s___s_e__", game.getVisibleSentence().toString());
+
+        game.guessedLetter('q');
+        assertEquals(1, game.getWrongGuess());
+
+        assertFalse(game.isGameIsWon());
+        assertFalse(game.isGameIsLost());
+    }
+
+    @Test
+    public void prøvHentOrdFraDr() throws Exception {
+        Logic spil = Logic.getInstance();
+        spil.hentOrdFraDr();
+        assertTrue("Mere end 100 ord fra DR", spil.getWordLibrary().size()>100);
     }
 }
