308e81404732e448585beda57984e8161d76773f
s175565 <s175565@student.dtu.dk>
2019-11-06 16:10:00 +0100

    PreferenceReader utils added

---
+++ app/src/main/java/com/example/galgeleg/util/PreferenceReader.java
@@ -0,0 +1,31 @@
+package com.example.galgeleg.util;
+
+
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * https://www.sundanesepeople.com/creating-onboarding-screen-android-studio/
+ */
+
+public class PreferenceReader {
+
+    private static final String PREFERENCES_FILE = "myPref";
+    private Context mContext;
+
+    public PreferenceReader(Context context) {
+        this.mContext = context;
+    }
+
+    public static String readSharedSetting(Context ctx, String settingName, String defaultValue) {
+        SharedPreferences sharedPref = ctx.getSharedPreferences(PREFERENCES_FILE, Context.MODE_PRIVATE);
+        return sharedPref.getString(settingName, defaultValue);
+    }
+    public static void saveSharedSetting(Context ctx, String settingName, String settingValue) {
+        SharedPreferences sharedPref = ctx.getSharedPreferences(PREFERENCES_FILE, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString(settingName, settingValue);
+        editor.apply();
+    }
+}
