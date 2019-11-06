783bbe5cf70f1dfcb4156b49e6a539babd37bf64
s175565 <s175565@student.dtu.dk>
2019-11-06 16:53:56 +0100

    Onboarding added

---
+++ app/src/main/java/com/example/galgeleg/activity/Onboarding.java
@@ -0,0 +1,134 @@
+package com.example.galgeleg.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Html;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.galgeleg.R;
+import com.example.galgeleg.adapter.SliderAdapter;
+import com.example.galgeleg.util.PreferenceReader;
+
+
+public class Onboarding extends AppCompatActivity implements View.OnClickListener {
+
+    private ViewPager mSlideViewPager;
+    private LinearLayout mDotLayout;
+    private SliderAdapter sliderAdapter;
+    private TextView[] mDots;
+    private Button mNextBtn, mSkipBtn;
+    private int mCurrentPage;
+    private final int NUM_OF_DOTS = 2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding);
+
+        mSlideViewPager = findViewById(R.id.slideViewPager);
+        mDotLayout = findViewById(R.id.dotsLayout);
+
+        mNextBtn = findViewById(R.id.nextbtn);
+        mSkipBtn = findViewById(R.id.skipbtn);
+
+        mNextBtn.setOnClickListener(this);
+        mSkipBtn.setOnClickListener(this);
+
+        sliderAdapter = new SliderAdapter(this);
+        mSlideViewPager.setAdapter(sliderAdapter);
+
+        addDotsIndicator(0);
+
+        mSlideViewPager.addOnPageChangeListener(viewListener);
+    }
+
+
+    public void addDotsIndicator(int position){
+        mDots = new TextView[NUM_OF_DOTS];
+        mDotLayout.removeAllViews();
+
+        for (int i = 0; i < NUM_OF_DOTS; i++){
+            mDots[i] = new TextView(this);
+            mDots[i].setText(Html.fromHtml("&#8226;"));
+
+
+            mDots[i].setTextSize(50);
+            mDots[i].setTextColor(getResources().getColor(R.color.white));
+
+            mDotLayout.addView(mDots[i]);
+        }
+
+        if (mDots.length > 0){
+            mDots[position].setTextColor(getResources().getColor(R.color.colorAccent));
+        }
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        boolean lastPage = mCurrentPage == mDots.length-1;
+
+        if (v == mNextBtn){
+            if (lastPage){
+                launchHearingTestScreen();
+            }
+            else {
+                mSlideViewPager.setCurrentItem(mCurrentPage + 1);
+            }
+        }
+        else if (v == mSkipBtn){
+            launchHearingTestScreen();
+        }
+    }
+
+    private void launchHearingTestScreen(){
+
+        PreferenceReader.saveSharedSetting(this, Menu.PREF_USER_FIRST_TIME, "false");
+
+        Intent intent = new Intent(this, Menu.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+        finish();
+
+
+    }
+
+    ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageSelected(int i) {
+            boolean firstPage = i == 0, lastPage = i == mDots.length-1;
+
+            addDotsIndicator(i);
+            mCurrentPage = i;
+
+            if (firstPage){
+                mNextBtn.setEnabled(true);
+                mNextBtn.setText("Next");
+            }
+            else if (lastPage){
+                mNextBtn.setEnabled(true);
+                mNextBtn.setText("Start");
+            }
+            else {
+                mNextBtn.setEnabled(true);
+                mNextBtn.setText("Next");
+            }
+        }
+
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) { }
+
+        @Override
+        public void onPageScrollStateChanged(int state) { }
+    };
+}
